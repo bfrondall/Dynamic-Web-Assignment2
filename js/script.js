88428644d831801615f9e0a972da59ee575e6acc
Blake Frondall <blake@Blakes-MacBook-Pro.local>
2021-03-23 10:57:46 -0600

    API Buttons

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
-// var http = require("https://open.spotify.com/playlist/7AcSLNFekKasTvg3Z8FiN0");
-
-// http.createServe(function(request, response){
-// 	response.writeHead(200, {"Content-Type": "text/plain"});
-// 	response.write("Hello World");
-// 	response.end();
-// }).listen(8888);
-
-
-//references https://developer.spotify.com/console/get-several-albums/
 var xhr = new XMLHttpRequest();
 // var url = "https://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases&fmt=json";
-var url = "https://api.lyrics.ovh/v1/Pink Floyd/ Money";
+var baseUrl = "https://api.lyrics.ovh/v1/";
+var query;
 
 
+	$("#Money").on("click", function(){
+		query = "Pink Floyd/ Money";
+		callAPI();
+	})
 
-	xhr.open('GET', url, true);
+	$("#WishYouWereHere").on("click", function(){
+		query = "Pink Floyd/ Wish You Were Here";
+		callAPI();
+	})
+
+	$("ComfortablyNumb").on("click", function(){
+		query = "Pink Floyd/ Comfortably Numb";
+		callAPI();
+	})
+
+function callAPI(){
+
+	var url = baseUrl + query;
+
+
+	xhr.open('GET', baseUrl, true);
 
 	xhr.send(null);
 
-	// app.get('/login', function(req, res) {
-	// var scopes = 'user-read-private user-read-email';
-	// res.redirect('https://accounts.spotify.com/authorize' + '?response_type=code' + '&client_id=' + my_client_id + (scopes ? '&scope=' + encodeURIComponent(scopes) : '') + '&redirect_uri=' + encodeURIComponent(redirect_uri));
-	// });
-
 	xhr.onload =function(){
 
-		if (xhr.status == 200) {
 
-			// document.write(xhr.responseText)
+		if (xhr.status == 200) {
 
 			var data = JSON.parse(xhr.responseText)
 			
 			console.log(data)
 
-			$("body").prepend("<p>" + data.lyrics + "</p>");
-			// var title = data.title;
-			// $("body").text(title);
+			$(".pod").prepend("<p>" + data.lyrics + "</p>");
 		}
 	}
+}
