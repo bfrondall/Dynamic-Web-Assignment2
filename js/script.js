b2a93bb0f909918161ab8cb9d53ff2bda8be8166
Blake Frondall <blake@Blakes-MacBook-Pro.local>
2021-03-22 19:45:48 -0600

    Finally got an API working

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@
 
 //references https://developer.spotify.com/console/get-several-albums/
 var xhr = new XMLHttpRequest();
-var url = "https://api.spotify.com/album/79dL7FLiJFOO0EoehUHQBv";
+// var url = "https://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?inc=aliases&fmt=json";
+var url = "https://api.lyrics.ovh/v1/Pink Floyd/ Money";
+
 
 
 	xhr.open('GET', url, true);
@@ -24,8 +26,15 @@ var url = "https://api.spotify.com/album/79dL7FLiJFOO0EoehUHQBv";
 	xhr.onload =function(){
 
 		if (xhr.status == 200) {
-			var data = JSON.parse(xhr.responseText);
 
-			console.log(data);
+			// document.write(xhr.responseText)
+
+			var data = JSON.parse(xhr.responseText)
+			
+			console.log(data)
+
+			$("body").prepend("<p>" + data.lyrics + "</p>");
+			// var title = data.title;
+			// $("body").text(title);
 		}
 	}
